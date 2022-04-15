188a5dafec2470419f58ec69cb9f4f8e0a9df620
Joe <54695266+jdboris@users.noreply.github.com>
2022-04-15 08:13:35 -0400

    Add AggroArea to Skeleton and set default target

--- modules/melee_ai_input.gd
+++ modules/melee_ai_input.gd
@@ -1,10 +1,10 @@
 extends "res://modules/ai_input/ai_input.gd"
 
+func _ready() -> void:
+	target = (get_node("../../Mage") as KinematicBody) 
+
 func _process(_delta: float):
-	var mob = (owner as KinematicBody)
-	var mage = (get_node("../../Mage") as KinematicBody)
-	var distance = mob.global_transform.origin.distance_to(mage.global_transform.origin)
-	target = mage if not target and distance < 8 else target
 	
 	if target:
+		var mob = (owner as KinematicBody)
 		move_mob_towards_target(mob, target)
