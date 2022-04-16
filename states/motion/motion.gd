08808e802bbd1f19cdbd2a2448f4c9eab6f4ce86
Joe <54695266+jdboris@users.noreply.github.com>
2022-04-16 08:04:07 -0400

    Change to global_transform.origin in update_look_direction

--- states/motion/motion.gd
+++ states/motion/motion.gd
@@ -2,4 +2,4 @@ extends "res://modules/state_machine/state.gd"
 
 func update_look_direction(direction: Vector3):
 	if direction:
-		owner.look_at(owner.translation - direction, Vector3.UP)
+		owner.look_at(owner.global_transform.origin - direction, Vector3.UP)
