7e3e40b95644e99853e934d4e3dca60dda50a995
Joe <54695266+jdboris@users.noreply.github.com>
2022-04-21 05:38:23 -0400

    Add time_left to casting state

--- states/combat/casting.gd
+++ states/combat/casting.gd
@@ -1,6 +1,7 @@
 extends "res://modules/state_machine/state.gd"
 
 var cast_animation: String
+var time_left: float setget , _get_time_left
 
 func _ready() -> void:
 	is_reversible = true
@@ -19,3 +20,7 @@ func enter(args := {"spell": {}, "target_pos": Vector2.ZERO}):
 func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
 	if anim_name == cast_animation:
 		emit_signal("finished", null, {})
+
+func _get_time_left():
+	var player := (get_node(animation_player) as AnimationPlayer)
+	return player.current_animation_length - player.current_animation_position
