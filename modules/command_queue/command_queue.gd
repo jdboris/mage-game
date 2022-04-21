2db15973afbf3e71fc8bbac7e08eeac2deef24fc
Joe <54695266+jdboris@users.noreply.github.com>
2022-04-21 07:02:35 -0400

    Refactor/rename

--- modules/command_queue/command_queue.gd
+++ modules/command_queue/command_queue.gd
@@ -16,10 +16,10 @@
 extends Node
 const Command: = preload("command.gd")
 
-var _promises := []
+var queue := []
 
 func new_command() -> Command:
-	var previous = _promises.back() if not _promises.empty() else null
+	var previous = queue.back() if not queue.empty() else null
 	var root = _empty_promise()
 	_promise_command()
 	_push_promise(root)
@@ -27,16 +27,16 @@ func new_command() -> Command:
 	return Command.new(previous, root)
 
 func _promise_command():
-	if _promises.size():
-		yield(_promises.back(), "completed")
+	if queue.size():
+		yield(queue.back(), "completed")
 
 func _push_promise(promise: GDScriptFunctionState):
-	_promises.push_back(promise)
+	queue.push_back(promise)
 	var error: = promise.connect("completed", self, "_pop_promise")
 	assert(!error, "Error: Failed to connect to '_pop_promise' signal.")
 
 func _pop_promise():
-	return _promises.pop_front()
+	return queue.pop_front()
 
 func _empty_promise():
 	yield()
